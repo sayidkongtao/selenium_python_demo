fc17479d641578a99f124ce369ee7eedde035cbc
Sayid <kongtao01@beyondsoft.com>
2020-11-30 10:05:50 +0800

    update code

--- common/basepage.py
+++ common/basepage.py
@@ -6,23 +6,42 @@ class BasePage(object):
     def __init__(self, driver, base_url=""):
         self.driver = driver
         self.base_url = base_url
-        self.time_out = 30
-        self.webdriver_wait = WebDriverWait(self.driver, self.time_out)
 
-    def click(self, element):
+    @staticmethod
+    def click(element):
         element.click()
 
-    def send_text(self, element, value):
+    @staticmethod
+    def send_text(element, value):
         element.send_keys(value)
 
-    def get_text(self, element):
+    @staticmethod
+    def get_text(element):
         return element.text
 
-    def wait_for_element_visible(self, locator):
-        return self.webdriver_wait.until(EC.visibility_of_element_located(locator))
+    def wait_for_element_visible(self, locator, time_out=30, poll_frequency=0.5, ignored_exceptions=None):
+        driver_wait = WebDriverWait(self.driver, time_out, poll_frequency=poll_frequency,
+                                    ignored_exceptions=ignored_exceptions)
+        return driver_wait.until(EC.visibility_of_element_located(locator))
 
-    def wait_for_element_presence(self, locator):
-        return self.webdriver_wait.until(EC.presence_of_element_located(locator))
+    def wait_for_element_presence(self, locator, time_out=30, poll_frequency=0.5, ignored_exceptions=None):
+        driver_wait = WebDriverWait(self.driver, time_out, poll_frequency=poll_frequency,
+                                    ignored_exceptions=ignored_exceptions)
+        return driver_wait.until(EC.presence_of_element_located(locator))
+
+    def wait_for_element_invisible(self, locator, time_for_first_visible=5, time_out=30, poll_frequency=0.5,
+                                   ignored_exceptions=None):
+        driver_wait = WebDriverWait(self.driver, time_for_first_visible)
+        try:
+            driver_wait.until(EC.presence_of_element_located(locator))
+            self.webdriver_wait.until_not(EC.presence_of_element_located(locator))
+        except:
+            # ignore the error
+            pass
+
+        driver_wait = WebDriverWait(self.driver, time_out, poll_frequency=poll_frequency,
+                                    ignored_exceptions=ignored_exceptions)
+        driver_wait.until_not(EC.presence_of_element_located(locator))
 
     def is_element_visible(self, locator):
         try:
@@ -32,5 +51,3 @@ class BasePage(object):
 
     def find_elements(self, locator):
         return self.driver.find_elements(locator[0], locator[1])
-
-
